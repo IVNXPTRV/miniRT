5f23e82eb5c2c453aa578266ed0ba6e48866c54d
Ivan <ivanpetrov.main@gmail.com>
2025-06-02 11:30:37 +0700

    feat: add all dirs automatically to run.sh; add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+## How to Run
+
+1. Compile the program:
+	```bash
+	make
+	```
+
+2. Run the executable:
+	```bash
+	./run.sh
+	```
+
+To understand more, switch to Notes brunch and read inline comments. Notes brunch is incomplete and untested, but code is more verbous.
--- run.sh
+++ run.sh
@@ -4,22 +4,28 @@
 
 # Check if a folder is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <folder>"
-  exit 1
+  FOLDERS=(
+    "./scenes/basic/"
+    "./scenes/nice/"
+    "./scenes/other/"
+    "./scenes/error/"
+  )
+else
+  FOLDERS=("$1")
 fi
 
-FOLDER="$1"
+TARGET="./scenes/error/"
 
-# Check if folder exists
-if [ ! -d "$FOLDER" ]; then
-  echo "Error: '$FOLDER' is not a directory"
-  exit 1
-fi
+found=false
+for folder in "${FOLDERS[@]}"; do
+  if [[ "$folder" == "$TARGET" ]]; then
+    found=true
+    break
+  fi
+done
 
-# Get basename of folder to check if it is "error"
-BASENAME=$(basename "$FOLDER")
-
-if [ "$BASENAME" = "error" ]; then
+if $found; then
+   touch "./scenes/error/nopermission.rt"
    chmod -r "./scenes/error/nopermission.rt"
    CMDS=(
      "./miniRT ./scenes/error/nonexistingfile.rt"
@@ -46,29 +52,31 @@ if [ "$BASENAME" = "error" ]; then
    done
 fi
 
-for FILE in "$FOLDER"/*; do
-  if [ -f "$FILE" ]; then
-    if [ "$BASENAME" = "error" ]; then
-      echo -e "Running valgrind on: $FILE\n"
-      valgrind --track-fds=yes --leak-check=full -q ./miniRT "$FILE" &
-      PID1=$!
-    else
-      echo -e "Opening: $FILE\n"
-      ./miniRT "$FILE" &
-      PID1=$!
-    fi
-      echo -e "Press Enter to close and open next, or 'q' to quit...\n"
-      read -r -n1 KEY
-      echo
+for FOLDER in "${FOLDERS[@]}"; do
+  for FILE in "$FOLDER"/*; do
+    if [ -f "$FILE" ]; then
+       if [ "$BASENAME" = "error" ]; then
+         echo -e "Running valgrind on: $FILE\n"
+         valgrind --track-fds=yes --leak-check=full -q ./miniRT "$FILE" &
+         PID1=$!
+       else
+         echo -e "Opening: $FILE\n"
+         ./miniRT "$FILE" &
+         PID1=$!
+       fi
+         echo -e "Press Enter to close and open next, or 'q' to quit...\n"
+         read -r -n1 KEY
+         echo
 
-      kill $PID1 2>/dev/null
-      wait $PID1 2>/dev/null
+         kill $PID1 2>/dev/null
+         wait $PID1 2>/dev/null
 
-      if [ "$KEY" = "q" ]; then
-        echo "Exiting."
-        break
-      fi
-    fi
+         if [ "$KEY" = "q" ]; then
+           echo "Exiting."
+           break
+         fi
+       fi
+   done
 done
-
+rm  "./scenes/error/nopermission.rt"
 echo "All scenes are shown. Exiting..."
